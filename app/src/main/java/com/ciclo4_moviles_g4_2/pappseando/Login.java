a4559cb33e0e0cf896a97489a29037069f87afe3
jmcaicedog <juanmanuelcaicedo@hotmail.com>
2021-11-22 12:56:04 -0500

    Inserción del acticity de mapas y conexión de la navegación con los otros activities

--- app/src/main/java/com/ciclo4_moviles_g4_2/pappseando/Login.java
+++ app/src/main/java/com/ciclo4_moviles_g4_2/pappseando/Login.java
@@ -2,7 +2,12 @@ package com.ciclo4_moviles_g4_2.pappseando;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.ciclo4_moviles_g4_2.pappseando.list_adapter.PlacesAdapter;
 
 public class Login extends AppCompatActivity {
 
@@ -10,5 +15,13 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Button inicia_sesion = findViewById(R.id.btn_inicio);
+        inicia_sesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Login.this,MapsActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/ciclo4_moviles_g4_2/pappseando/MapsActivity.java
@@ -0,0 +1,292 @@
+package com.ciclo4_moviles_g4_2.pappseando;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnMyLocationButtonClickListener, GoogleMap.OnMyLocationClickListener {
+
+    private GoogleMap mMap;
+    private final int REQUEST_LOCATION_PERMISSION = 1;
+    private static final String TAG = MapsActivity.class.getSimpleName();
+    private CameraPosition cameraPosition;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private final LatLng defaultLocation = new LatLng(3.273191, -76.504462);
+    private static final int DEFAULT_ZOOM = 15;
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
+    private boolean locationPermissionGranted;
+    private Location lastKnownLocation;
+    private static final String KEY_CAMERA_POSITION = "camera_position";
+    private static final String KEY_LOCATION = "location";
+
+    private MapView mapView;
+
+    private static final String MAP_VIEW_BUNDLE_KEY = "AIzaSyDvdzT2bfN6yWjcAdxC75B5OLYHc1f07WY";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+
+        FloatingActionButton btnagregar = (FloatingActionButton) findViewById(R.id.btnagregar);
+        Button agregar = findViewById(R.id.button);
+
+
+        agregar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mensaje();
+                Intent i = new Intent(MapsActivity.this,ListViewActivity.class);
+                startActivity(i);
+            }
+        });
+
+        btnagregar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mensaje();
+                addcurrentlocation();
+            }
+        });
+
+        Bundle mapViewBundle = null;
+
+        if (savedInstanceState != null) {
+            mapViewBundle = savedInstanceState.getBundle(MAP_VIEW_BUNDLE_KEY);
+            lastKnownLocation = savedInstanceState.getParcelable(KEY_LOCATION);
+            cameraPosition = savedInstanceState.getParcelable(KEY_CAMERA_POSITION);
+        }
+
+        mapView = findViewById(R.id.map_view);
+        mapView.onCreate(mapViewBundle);
+        mapView.getMapAsync(this);
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+    }
+
+    public void mensaje() {
+        Toast.makeText(this,"Marcador creado",Toast.LENGTH_SHORT).show();
+    }
+
+    private void getDeviceLocation() {
+
+        try {
+            if (locationPermissionGranted) {
+                Task<Location> locationResult = fusedLocationProviderClient.getLastLocation();
+                locationResult.addOnCompleteListener(this, new OnCompleteListener<Location>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Location> task) {
+                        if (task.isSuccessful()) {
+                            lastKnownLocation = task.getResult();
+                            if (lastKnownLocation != null) {
+                                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                                        new LatLng(lastKnownLocation.getLatitude(),
+                                                lastKnownLocation.getLongitude()), DEFAULT_ZOOM));
+                            }
+                        } else {
+                            Log.d(TAG, "La ubicación actual es nula, valores por defecto.");
+                            Log.e(TAG, "Excepción: %s", task.getException());
+                            mMap.moveCamera(CameraUpdateFactory
+                                    .newLatLngZoom(defaultLocation, DEFAULT_ZOOM));
+                            mMap.getUiSettings().setMyLocationButtonEnabled(false);
+                        }
+                    }
+                });
+            }
+        } catch (SecurityException e)  {
+            Log.e("Excepción: %s", e.getMessage(), e);
+        }
+    }
+
+
+    private void addcurrentlocation() {
+
+        EditText etNombre = findViewById(R.id.etNombre);
+
+        try {
+            if (locationPermissionGranted) {
+                Task<Location> locationResult = fusedLocationProviderClient.getLastLocation().addOnSuccessListener(this, location ->
+                {
+                    if (location != null) {
+                        mMap.addMarker(new MarkerOptions().position(new LatLng(location.getLatitude(), location.getLongitude())).title(etNombre.getText().toString()));
+
+
+
+                        FirebaseDatabase database = FirebaseDatabase.getInstance();
+                        DatabaseReference myRef = database.getReference("Ubicación");
+
+                        Ubication ub = new Ubication(String.valueOf(location.getLatitude()), String.valueOf(location.getLongitude()));
+
+                        myRef.child(etNombre.getText().toString()).setValue(ub);
+                        etNombre.setText("");
+
+
+
+                    }
+                });
+            }
+        } catch (SecurityException e)  {
+            Log.e("Excepción: %s", e.getMessage(), e);
+        }
+    }
+
+    private void getLocationPermission() {
+
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            locationPermissionGranted = true;
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        locationPermissionGranted = false;
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    locationPermissionGranted = true;
+                }
+            }
+        }
+        updateLocationUI();
+    }
+
+    private void updateLocationUI() {
+        if (mMap == null) {
+            return;
+        }
+        try {
+            if (locationPermissionGranted) {
+                mMap.setMyLocationEnabled(true);
+                mMap.getUiSettings().setMyLocationButtonEnabled(true);
+            } else {
+                mMap.setMyLocationEnabled(false);
+                mMap.getUiSettings().setMyLocationButtonEnabled(false);
+                lastKnownLocation = null;
+                getLocationPermission();
+            }
+        } catch (SecurityException e)  {
+            Log.e("Exception: %s", e.getMessage());
+        }
+    }
+
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        getLocationPermission();
+
+        updateLocationUI();
+
+        getDeviceLocation();
+
+
+    }
+
+    @Override
+    public boolean onMyLocationButtonClick() {
+        return false;
+    }
+
+    @Override
+    public void onMyLocationClick(@NonNull Location location) {
+        Toast.makeText(this,"Marcador agregado",Toast.LENGTH_LONG).show();
+    }
+
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        Bundle mapViewBundle = outState.getBundle(MAP_VIEW_BUNDLE_KEY);
+        if (mapViewBundle == null) {
+            mapViewBundle = new Bundle();
+            outState.putBundle(MAP_VIEW_BUNDLE_KEY, mapViewBundle);
+        }
+
+        mapView.onSaveInstanceState(mapViewBundle);
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+    @Override
+    protected void onPause() {
+        mapView.onPause();
+        super.onPause();
+    }
+    @Override
+    protected void onDestroy() {
+        mapView.onDestroy();
+        super.onDestroy();
+    }
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+
+}
+
+class Ubication{
+    public String latitud;
+    public String logitud;
+
+    public Ubication(String latitud, String logitud){
+        this.latitud = latitud;
+        this.logitud = logitud;
+    }
+
+
+}
